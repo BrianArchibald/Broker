90cac28fbb947fd0a4ab17ed9ea5d3147d0ddbcd
BrianArchibald <brianallenarchibald@gmail.com>
2018-08-07 19:25:30 -0500

    added js, pics, and css

---
+++ broker.js
@@ -0,0 +1,15 @@
+
+const counter1 = document.getElementById("counter1");
+const counter2 = document.getElementById("counter2");
+const counter3 = document.getElementById("counter3");
+const counter4 = document.getElementById("counter4");
+
+counter1.textContent = "1";
+counter2.textContent = "1";
+counter3.textContent = "1";
+counter4.textContent = "1";
+
+
+if (window.scrollY > 120) {
+	
+}
