6911eb16751d333c5d7eb6dc28d3d4d3f92b7a44
BrianArchibald <brianallenarchibald@gmail.com>
2018-08-10 20:58:56 -0500

    fixed js counter issue, removed bootstrap js

--- broker.js
+++ broker.js
@@ -1,44 +1,25 @@
-const nav = document.getElementById("navbar");
-const topOfNav = nav.offsetTop;
-
-
 const counter1 = document.getElementById("counter1");
 const counter2 = document.getElementById("counter2");
 const counter3 = document.getElementById("counter3");
 const counter4 = document.getElementById("counter4");
-const counterRow = document.getElementById("counter-row");
 
 counter1.textContent = "1";
 counter2.textContent = "1";
 counter3.textContent = "1";
 counter4.textContent = "1";
-
-
-	 
-// function startCounting() {
-// 	if (window.scrollY > 120) {
-
-// 		setInterval(function() {
-// 	if(count1 <= 2500) {
-// 		counter1.textContent = count1;
-// 		count1 +=5;
-// 	},100);
-// };
-	
+  
 let firstTimer = 0;
 let secondTimer = 0;
 let thirdTimer = 0;
 let fourthTimer = 0;
 
-
-
 function startCounting() {
-	if(window.scrollY >= topOfNav) {
+  
     counter1.textContent = firstTimer;
     let brokerTimer = setInterval(function(){
       if(firstTimer <= 550) {
         counter1.textContent = firstTimer;
-        firstTimer+=6;
+        firstTimer+=3;
       } else {
         clearInterval(firstTimer);
       }
@@ -47,8 +28,8 @@ function startCounting() {
     counter2.textContent = secondTimer;
     let transactionsTimer = setInterval(function(){
       if(secondTimer <= 91720) {
-        counter1.textContent = secondTimer;
-        secondTimer+=87;
+        counter2.textContent = secondTimer;
+        secondTimer+=287;
       } else {
         clearInterval(secondTimer);
       }
@@ -57,7 +38,7 @@ function startCounting() {
      counter3.textContent = thirdTimer;
     let clientsTimer = setInterval(function(){
       if(thirdTimer <= 3350) {
-        counter1.textContent = thirdTimer;
+        counter3.textContent = thirdTimer;
         thirdTimer+=11;
       } else {
         clearInterval(thirdTimer);
@@ -73,13 +54,9 @@ function startCounting() {
         clearInterval(fourthTimer);
       }
     }, 140);
-	}
   }
 
-
-
-
-nav.addEventListener("scroll", startCounting);
-	
-	
+window.addEventListener("scroll", startCounting);
+  
+  
 
